884dba2aa9e46b6dd6d83b16bfb4c52cc194bca3
danilo rocha <danilo.rochaa@gmail.com>
2021-09-09 18:32:30 -0300

    Add comentários.

--- src/main/java/StrategyComTemplateMethod/LeituraRetorno.java
+++ src/main/java/StrategyComTemplateMethod/LeituraRetorno.java
@@ -25,7 +25,7 @@ public interface LeituraRetorno {
                 Boleto boleto = new Boleto();
                 boleto.setId(Integer.parseInt(vetor[0]));
                 boleto.setCodBanco(vetor[1]);
-                preenchderDados(boleto, vetor);//metodo primitivo
+                preenchderDados(boleto, vetor);//operação primitiva
                 boletos.add(boleto);
             }
             return boletos;
@@ -35,7 +35,7 @@ public interface LeituraRetorno {
         }
     }//metodo
 
-    //Método primitivo
+    //Operação primitivo
     abstract void preenchderDados(Boleto boleto, String[] vetor);
 
 }//classe
